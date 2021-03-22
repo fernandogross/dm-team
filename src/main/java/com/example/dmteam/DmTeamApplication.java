2600463175a7709f859c170477a2e42a101665bc
Fernando Gross <marciogross@gmail.com>
2021-03-22 20:52:21 -0300

    First commit

---
+++ src/main/java/com/example/dmteam/DmTeamApplication.java
@@ -0,0 +1,13 @@
+package com.example.dmteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmTeamApplication.class, args);
+	}
+
+}
